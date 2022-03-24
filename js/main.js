4a6348e4b7f13b73e5af66966229efa3a230f3f8
Ali Komijani <akpa125@gmail.com>
2022-03-24 18:33:15 +0430

    fix scrolling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Burger menu
 const toggler = document.querySelector("#BurgerMenu_toggler");
-
 toggler.addEventListener("click", function () {
   let menu = document.querySelector(".BurgerMenu");
   menu.classList.toggle("BurgerMenu--hidden");
@@ -8,13 +7,40 @@ toggler.addEventListener("click", function () {
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-xmark");
 });
-
+let pos = {};
 // expendable
 const collapsibles = document.querySelectorAll(".collapsible");
-
 collapsibles.forEach(function (elem) {
   const collapsible_toggler = elem.querySelector(".collapsible__expand");
   collapsible_toggler.addEventListener("click", function (e) {
     this.parentElement.classList.toggle("collapsible--expanded");
   });
 });
+// dragable
+const app_lists = document.querySelectorAll(".app-list");
+app_lists.forEach(function (app_list) {
+  app_list.addEventListener("mousedown", mousedown);
+});
+function mousedown(e) {
+  e.preventDefault();
+  pos = {
+    // The current scroll
+    left: this.scrollLeft,
+    // Get the current mouse position
+    x: e.clientX,
+  };
+  this.onmousemove = mousemove;
+  this.onmouseup = mouseup;
+}
+function mousemove(e) {
+  e.preventDefault();
+  const dx = e.clientX - pos.x;
+  // Scroll the element
+  this.scrollLeft = pos.left - dx;
+}
+function mouseup(e) {
+  e.preventDefault();
+  console.log(e.target);
+  this.onmousemove = undefined;
+  this.onmouseup = undefined;
+}
