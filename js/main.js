8268ed8b4184551f93aad11e0fd4add1958148c7
Ali Komijani <akpa125@gmail.com>
2022-03-24 11:52:12 +0430

    add collapsible

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+// Burger menu
 const toggler = document.querySelector("#BurgerMenu_toggler");
+
 toggler.addEventListener("click", function () {
   let menu = document.querySelector(".BurgerMenu");
   menu.classList.toggle("BurgerMenu--hidden");
   let icon = toggler.querySelector(".fas");
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-xmark");
+});
+
+// expendable
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach(function (elem) {
+  const collapsible_toggler = elem.querySelector(".collapsible__expand");
+  collapsible_toggler.addEventListener("click", function (e) {
+    this.parentElement.classList.toggle("collapsible--expanded");
+  });
 });
